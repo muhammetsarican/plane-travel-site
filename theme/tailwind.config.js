0bd5438c09d6a22f35172cc38d4a5314d4ca06a6
muhammetsarican <muhammetsarican@gmail.com>
2024-09-18 22:47:56 +0300

    initial commit.

---
+++ theme/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/*.html"
+  ],
+  theme: {
+    container: {
+      center: true,
+    },
+    colors: {
+      "primary": "#E6DBFF",
+      "secondary": "#F6F4F8",
+      "primary-white": "#FFFFFF",
+      "primary-purple": "#4A0096",
+    },
+    extend: {
+    },
+  },
+  plugins: [],
+}
+
