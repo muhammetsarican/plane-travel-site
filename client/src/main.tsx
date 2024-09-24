9ed79f1df079c89c71168456c07e1ea594fee741
muhammetsarican <muhammetsarican@gmail.com>
2024-09-24 17:02:41 +0300

    main comp wrapped with new providers.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,13 +3,19 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from './router/index.tsx'
 import { FlightDataProvider } from './providers/FlightDataProvider.tsx'
 import { AxiosProvider } from './providers/AxiosProvider.tsx'
+import { UserFlightDataProvider } from './providers/UserFlightProvider.tsx'
+import { AuthProvider } from './providers/AuthProvider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <>
-    <AxiosProvider>
-      <FlightDataProvider>
-        <RouterProvider router={router} />
-      </FlightDataProvider>
-    </AxiosProvider>
+    <AuthProvider>
+      <AxiosProvider>
+        <FlightDataProvider>
+          <UserFlightDataProvider>
+            <RouterProvider router={router} />
+          </UserFlightDataProvider>
+        </FlightDataProvider>
+      </AxiosProvider>
+    </AuthProvider>
   </>,
 )
