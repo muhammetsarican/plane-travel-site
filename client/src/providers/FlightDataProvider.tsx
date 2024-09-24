6ca086e493ae17d92e6ef55ca7f03451437f3d4b
muhammetsarican <muhammetsarican@gmail.com>
2024-09-24 17:05:22 +0300

    refactor: new state field created for manage current flight object.

--- client/src/providers/FlightDataProvider.tsx
+++ client/src/providers/FlightDataProvider.tsx
@@ -4,16 +4,20 @@ import { useAxios } from "./AxiosProvider";
 type FlightDataContextType = {
     flightData: Array<any> | null,
     setFlightData(value: any): void,
+    currentFlight: any | null,
+    setCurrentFlight(value: any): void,
 };
 
 const FlightDataContext = createContext<FlightDataContextType>({
     flightData: [{}] || null,
     setFlightData: () => { },
-
+    currentFlight: {} || null,
+    setCurrentFlight: () => { },
 });
 
 const FlightDataProvider = ({ children }: { children: ReactNode }) => {
     const [flightData, setFlightData] = useState<Array<any> | null>(null);
+    const [currentFlight, setCurrentFlight] = useState<any | null>(null);
 
     const { client } = useAxios();
 
@@ -31,6 +35,8 @@ const FlightDataProvider = ({ children }: { children: ReactNode }) => {
     const values = {
         flightData,
         setFlightData,
+        currentFlight,
+        setCurrentFlight,
     };
 
     return <FlightDataContext.Provider value={values}>
